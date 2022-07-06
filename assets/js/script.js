b02486cff2c3e3c8dd4501cfc8bb72d1161b332f
hcweigand10 <henryweigand10@gmail.com>
2022-07-06 14:59:48 -0700

    demo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,60 @@
-var searchFormEl = document.querySelector('#search-form');
 
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
+var searchInputEl = document.getElementById("search-input")
+var formatInputEL = document.getElementById("format-input")
+var searchBtnEl = document.getElementById("search-btn")
 
-  var searchInputVal = document.querySelector('#search-input').value;
-  var formatInputVal = document.querySelector('#format-input').value;
+searchBtnEl.addEventListener("click", getParams)
 
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
-    return;
+
+// getParams
+function getParams (event) {
+  event.preventDefault()
+  var searchTerm = searchInputEl.value
+  var format = formatInputEL.value
+  if (searchTerm === "") {
+    alert("please enter something")
+    return
   }
-
-  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
-
-  location.assign(queryString);
+  apiSearch(searchTerm, format)
 }
 
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
 
-// testing comment from henry
+// apiSearch
+function apiSearch(search, format) {
+  var requestUrl = "https://www.loc.gov/"
+  requestUrl = requestUrl + format + "/?fo=json&q=" + search
+
+  fetch(requestUrl) 
+  .then( function (response) {
+    if (!response.ok) {
+      console.log("error")
+    } else {
+      return response.json()
+    }
+
+  })
+  .then(function (response) {
+    if (response) {
+      printData(response)
+    }
+  })
+
+}
+
+// printData
+function printData(data) {
+  // console.log(data.results[2])
+  for (let i = 0; i < 10; i++) {
+    const element = data.results[i];
+    const contentCard = document.createElement("div")
+    contentCard.setAttribute("class", "card mb-3 p-3 bg-light")
+    const contentTitle = document.createElement("h3")
+    contentTitle.textContent = `Title: ${element.title}`
+    const contentDate = document.createElement("h4")
+    contentDate.textContent = `Date: ${element.date}`
+    // const contentBody = document.createElement("p")
+    contentCard.append(contentTitle, contentDate)
+    document.getElementById("results-content").append(contentCard)
+    
+  }
+}
